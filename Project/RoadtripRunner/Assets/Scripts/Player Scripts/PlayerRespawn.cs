3ba5ae2f904568e022e01bb547df50df32ea20cd
AIE\s170958 <krianoto@gmail.com>
2017-10-23 15:11:00 +1030

    Respawn Script: Set tags on the respawn gameobject and the plane gameobject, then set the public strings on this script to those tags to ensure this script will work correctly.

---
+++ Project/RoadtripRunner/Assets/Scripts/Player Scripts/PlayerRespawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    //Note: Don't use on an object with a rigidbody. Rigidbody and character controller hate each other.
+
+    public GameObject SpawnPoint;
+    public string RespawnTag = "Respawn"; //Set this string to whatever the respawn object tag is
+    public string PlaneTag = "KillFloor"; //Set this string to whatever the plane object tag is
+
+
+    void Start()
+    {
+        SpawnPoint = GameObject.FindGameObjectWithTag(RespawnTag);
+    }
+
+    void Update()
+    {
+        Plane TestPlane = new Plane(new Vector3(0, 1, 0), GameObject.FindGameObjectWithTag(PlaneTag).transform.position);
+
+        if (TestPlane.GetSide(gameObject.transform.position) == false)
+            gameObject.transform.position = SpawnPoint.transform.position;
+    }
+}
