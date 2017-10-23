9f85fbafc8bab4d46be1a29dedc7abf9779946cb
Georgia Shepherd <fruitbat_08@hotmail.com>
2017-10-23 11:27:57 +1030

    Adding controller testing scene/begining of controller script

---
+++ Project/RoadtripRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	private CharacterController m_controller;
+	private Rigidbody m_rigidbody;
+	public float m_speed = 0;
+	public Vector3 m_jumpSpeed;
+
+	// Use this for initialization
+	void Start () {
+		CharacterController m_controller = GetComponent<CharacterController>();
+		m_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(m_controller.isGrounded == true){
+			print("grounded can't fump");
+		} else {
+			print("can jump");
+		}
+	}
+
+	void FixedUpdate() {
+		m_rigidbody.AddForce(new Vector3(m_speed, 0, 0));
+		if(Input.GetButtonDown("Jump") == true && m_controller.isGrounded == true){
+			m_rigidbody.AddForce(m_jumpSpeed, ForceMode.VelocityChange);
+		}
+	}
+}
