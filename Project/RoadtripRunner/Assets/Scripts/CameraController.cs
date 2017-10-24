c6e16335ca7c62f013a0d74d6cc4f40b333305eb
Georgia Shepherd <fruitbat_08@hotmail.com>
2017-10-25 01:03:02 +1030

    (changes from class monday)
    Fixing character controller acceleration/jump, added camera controller (follows player horizontally but not with jumps)

---
+++ Project/RoadtripRunner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+	public Transform m_player;
+
+	void Update() {
+		if(m_player != null) {
+			transform.position = new Vector3(m_player.position.x + 10, 7, -13);
+		}
+	}
+
+}
--- Project/RoadtripRunner/Assets/Scripts/PlayerController.cs
+++ Project/RoadtripRunner/Assets/Scripts/PlayerController.cs
@@ -5,30 +5,64 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
-	private CharacterController m_controller;
 	private Rigidbody m_rigidbody;
+	private bool isJumping;
+
 	public float m_speed = 0;
+	public float m_maxSpeed;
 	public Vector3 m_jumpSpeed;
+	private Vector3 TestVelocity;
 
 	// Use this for initialization
-	void Start () {
-		CharacterController m_controller = GetComponent<CharacterController>();
+	void Start() {
 		m_rigidbody = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(m_controller.isGrounded == true){
-			print("grounded can't fump");
-		} else {
-			print("can jump");
-		}
+		isJumping = false;
 	}
 
+	//maybe swap back to char cont. for const. movement
 	void FixedUpdate() {
-		m_rigidbody.AddForce(new Vector3(m_speed, 0, 0));
-		if(Input.GetButtonDown("Jump") == true && m_controller.isGrounded == true){
-			m_rigidbody.AddForce(m_jumpSpeed, ForceMode.VelocityChange);
+		//constant  +x movement clamped to maxSpeed
+		//m_rigidbody.velocity = new Vector3(m_speed, m_jumpSpeed.y, 0);
+		//m_rigidbody.velocity = new Vector3 (Mathf.Clamp(m_rigidbody.velocity.x, m_speed, m_maxSpeed), 0, 0);
+
+		//take 2
+
+
+
+		//clamping only x value
+		//Vector3 localVelocity = transform.InverseTransformPoint(m_rigidbody.velocity);
+		//localVelocity.x = Mathf.Clamp(m_rigidbody.velocity.x * Time.deltaTime, m_speed, m_maxSpeed);
+		//m_rigidbody.velocity = transform.TransformPoint(localVelocity);
+
+		//jump if space pressed And currently grounded
+		if(Input.GetButtonDown("Jump") && isJumping == false) {
+			Debug.Log("WEEEE");
+			m_rigidbody.AddForce(new Vector3(0, m_jumpSpeed.y, 0), ForceMode.Impulse);
+			//m_rigidbody.velocity += new Vector3(0, m_jumpSpeed * Time.deltaTime, 0);
 		}
+
+		m_rigidbody.velocity *= Time.deltaTime;
+
+		if(m_rigidbody.velocity.x >= m_maxSpeed) {
+			m_rigidbody.velocity = new Vector3(m_maxSpeed, m_rigidbody.velocity.y, m_rigidbody.velocity.z);
+		} else {
+			m_rigidbody.velocity += new Vector3(m_speed, 0, 0);
+		}
+
+		//m_rigidbody.AddForce(m_rigidbody.velocity);
+
+
+
+		//delete later, for testing purposes
+		TestVelocity = m_rigidbody.velocity;
+	}
+
+	//not currently using isGrounded due to no fast/easy way to add gravity while also allowing jumping
+	void OnCollisionEnter(Collision hit) {
+		isJumping = false;
+	}
+
+	void OnCollisionExit(Collision hit) {
+		isJumping = true;
 	}
 }
