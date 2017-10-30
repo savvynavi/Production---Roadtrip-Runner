62fb635c1b3d686dbe7870e9d894c18daf82c518
AIE\s170958 <krianoto@gmail.com>
2017-10-30 16:36:14 +1030

    Minor Changes: Menu Screen

--- Project/RoadtripRunner/Assets/Scripts/Menu Scripts/MenuControl.cs
+++ Project/RoadtripRunner/Assets/Scripts/Menu Scripts/MenuControl.cs
@@ -8,17 +8,23 @@ public class MenuControl : MonoBehaviour
 {
     public GameObject mainPanel;
     public GameObject settingsPanel;
+    public GameObject upgradePanel;
+    public bool audioToggle;
 
     void Start()
     {
         mainPanel.SetActive(true);
         settingsPanel.SetActive(false);
+        upgradePanel.SetActive(false);
     }
 
     public void Play_OnClick()
     {
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+        upgradePanel.SetActive(false);
         //This loads a test scene. To add a scene, go into File > Build Settings and drop a scene in. Then, put the corresponding number below (first scene is 0, second 1, etc)
-        SceneManager.LoadScene(1, LoadSceneMode.Single); 
+        SceneManager.LoadScene(1, LoadSceneMode.Additive); 
     }
 
     public void Settings_OnClick()
@@ -27,9 +33,32 @@ public class MenuControl : MonoBehaviour
         settingsPanel.SetActive(true);
     }
 
-    public void Back_OnClick()
+    public void SBack_OnClick()
     {
         mainPanel.SetActive(true);
         settingsPanel.SetActive(false);
+    }
+
+    public void Upgrade_OnClick()
+    {
+        mainPanel.SetActive(false);
+        upgradePanel.SetActive(true);
+    }
+
+    public void UBack_OnClick()
+    {
+        mainPanel.SetActive(true);
+        upgradePanel.SetActive(false);
+    }
+
+    public void Audio_OnToggle()
+    {
+        audioToggle = !audioToggle;
+
+        //Will be uncommented when sound is implemented
+        //if (audioToggle == true)
+        //    audioSource.SetActive(true);
+        //else
+        //    audioSource.SetActive(false);
     }
 }
