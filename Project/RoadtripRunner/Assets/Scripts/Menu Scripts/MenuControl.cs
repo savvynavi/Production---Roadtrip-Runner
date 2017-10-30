d8fc3b9fa8b04aaf57ef568db072961e3a71c484
AIE\s170958 <krianoto@gmail.com>
2017-10-30 15:13:18 +1030

    Redone: Respawn script. Better functionality, last Point the player touches becomes the new active Respawn Point. Mostly Done: Collectable Spawner script, very basic Menu Screen. Collectable Spawns have a chance of spawning a collectable, collectable is destroyed on Player collision, no value added to player just yet. Menu Screen can switch between options and back, can load a scene to play.

---
+++ Project/RoadtripRunner/Assets/Scripts/Menu Scripts/MenuControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//SCRIPT EFFECT:
+//Allows user to play game (Play button), select settings (Settings button), go back to Main Menu from Settings (Back button).
+
+public class MenuControl : MonoBehaviour
+{
+    public GameObject mainPanel;
+    public GameObject settingsPanel;
+
+    void Start()
+    {
+        mainPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void Play_OnClick()
+    {
+        //This loads a test scene. To add a scene, go into File > Build Settings and drop a scene in. Then, put the corresponding number below (first scene is 0, second 1, etc)
+        SceneManager.LoadScene(1, LoadSceneMode.Single); 
+    }
+
+    public void Settings_OnClick()
+    {
+        mainPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void Back_OnClick()
+    {
+        mainPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+}
---
+++ Project/RoadtripRunner/Assets/Scripts/Object Scripts/TestMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        GetComponent<CharacterController>().SimpleMove(new Vector3(-6, 0, 0));
+    }
+}
