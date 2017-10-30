564d667abd22bca595a04de8e3387725e3a73519
Georgia Shepherd <fruitbat_08@hotmail.com>
2017-10-30 15:01:06 +1030

    flight controls added/pickups done

---
+++ Project/RoadtripRunner/Assets/Scripts/Pickups/upgradeFlight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeFlight : MonoBehaviour {
+	public PlayerController m_player;
+	public float m_timer;
+
+	// Use this for initialization
+	void Start() {
+		m_player.GetComponent<PlayerController>();
+	}
+
+	void OnTriggerEnter(Collider hit) {
+		if(hit.tag == "player") {
+			m_player.FlightUpgrade = true;
+			m_player.Timer = m_timer;
+			Destroy(this.gameObject);
+		}
+	}
+
+}
