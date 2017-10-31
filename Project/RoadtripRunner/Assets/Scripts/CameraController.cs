2bd6a449071f5f075682910f1e7164e0e43dcdf8
Michael Matthews <mmatthewspatch1@gmail.com>
2017-10-31 11:16:52 +1030

    Camera Controller Updated to look and have adjustable angles and stuff

--- Project/RoadtripRunner/Assets/Scripts/CameraController.cs
+++ Project/RoadtripRunner/Assets/Scripts/CameraController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 	public Transform m_player;
+	public float xThing;
+	public float yThing;
+	public float zThing;
 
 	//will follow given objects x-position, but not y or z(won't move up/down with jumps)
 	void Update() {
 		if(m_player != null) {
-			transform.position = new Vector3(m_player.position.x + 10, 7, -13);
+			transform.position = new Vector3(m_player.position.x - xThing, yThing, zThing);
+			transform.LookAt (m_player);
 		}
 	}
 
