9aefd539da25008289492115b01fbdb7ea6f0eda
Ashleigh Tyler <krianoto@gmail.com>
2017-11-07 10:58:00 +1030

    Changes to SegmentControl, should be easier to use. Set the end segment child of a platform to a tag, then enter the tag on the Unity display for the script.

--- Project/RoadtripRunner/Assets/Prefabs/Ash (Programmer)/Object Scripts/SegmentControl.cs
+++ Project/RoadtripRunner/Assets/Prefabs/Ash (Programmer)/Object Scripts/SegmentControl.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class SegmentControl : MonoBehaviour
 {
-    public GameObject prefabSegment1;
-    public GameObject prefabSegment2;
-    public GameObject prefabSegment3;
-
     public GameObject[] prefabSegments;
     private List<GameObject> segmentObjects = new List<GameObject>();
     public int segmentIndex = 0;
@@ -16,8 +13,10 @@ public class SegmentControl : MonoBehaviour
     public Vector3 firstSegmentPosition;
 
     public string cameraTag = "MainCamera";
+    public string endTag = "End";
+    public float distanceBehindCam = 10;
 
-    void Start ()
+    void Start()
     {
         mainCamera = GameObject.FindGameObjectWithTag(cameraTag).GetComponent<Camera>();
         SetLoadPos();
@@ -25,14 +24,13 @@ public class SegmentControl : MonoBehaviour
         foreach (GameObject prefab in prefabSegments)
             segmentObjects.Add(Instantiate(prefab, loadPos, new Quaternion()));
 
-        segmentIndex = Random.Range(0, prefabSegments.Length);
-        segmentObjects[segmentIndex].transform.position = firstSegmentPosition;
+        segmentObjects[segmentIndex = SetRandIndex(-1)].transform.position = firstSegmentPosition;
     }
 	
-	void Update ()
+	void Update()
     {
         SetLoadPos();
-        Transform target = segmentObjects[segmentIndex].gameObject.transform.GetChild(1);
+        Transform target = GetTransformOfChild(segmentObjects[segmentIndex]);
         Vector3 viewPos = mainCamera.WorldToViewportPoint(target.position);
         if (viewPos.x < 1)
             PlaceNextSegment(target.position);
@@ -41,18 +39,38 @@ public class SegmentControl : MonoBehaviour
     private void SetLoadPos()
     {
         loadPos = mainCamera.transform.position;
-        loadPos.z -= 10;
+        loadPos.z -= distanceBehindCam;
     }
 
     private void PlaceNextSegment(Vector3 SpawnToPos)
     {
         int lastIndex = segmentIndex;
-        segmentIndex = Random.Range(0, prefabSegments.Length);
-
-        if (segmentIndex == lastIndex)
-            while (segmentIndex == lastIndex)
-                segmentIndex = Random.Range(0, prefabSegments.Length);
+        segmentIndex = SetRandIndex(lastIndex);
 
         segmentObjects[segmentIndex].transform.position = SpawnToPos;
+
+        for (int i = 0; i < segmentObjects.Count; i++)
+            if (i != lastIndex && i != segmentIndex)
+                segmentObjects[i].transform.position = loadPos;
+    }
+
+    //Sets index to rand if it does not equal last index, returns last index.
+    private int SetRandIndex(int NumberOtherThan)
+    {
+        while (true)
+        {
+            int RandNum = Random.Range(0, prefabSegments.Length);
+            if (RandNum != NumberOtherThan)
+                return RandNum;
+        }
+    }
+
+    public Transform GetTransformOfChild(GameObject Segment)
+    {
+        for (int i = 0; i < Segment.transform.childCount; i++)
+            if (Segment.transform.GetChild(i).tag == endTag)
+                return Segment.transform.GetChild(i).transform;
+
+        return null;
     }
 }
--- Project/RoadtripRunner/Assets/Prefabs/Ash (Programmer)/Test & Temporary Scripts/TestCamera.cs
+++ Project/RoadtripRunner/Assets/Prefabs/Ash (Programmer)/Test & Temporary Scripts/TestCamera.cs
@@ -4,8 +4,13 @@ public class TestCamera : MonoBehaviour
 {
     public GameObject player;
 	
-	void Update ()
+    void Start()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -20);
+        transform.Rotate(new Vector3(10, 1, 0), 2);
+    }
+
+    void Update ()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 4, -20);
 	}
 }
