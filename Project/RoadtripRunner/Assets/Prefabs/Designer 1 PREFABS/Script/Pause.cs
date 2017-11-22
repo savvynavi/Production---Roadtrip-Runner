86efdce0d1e00ea9d4da087871a9e7c24e54c7b3
Michael Matthews <mmatthewspatch1@gmail.com>
2017-11-22 11:54:52 +1030

    Changed Lighting and added hedges to prefab segment, added pause function. player is now a prefab.

---
+++ Project/RoadtripRunner/Assets/Prefabs/Designer 1 PREFABS/Script/Pause.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	bool paused;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape) && !paused) {
+			Time.timeScale = 0f;
+			paused = true;
+			// show menu
+		} else if (Input.GetKeyDown (KeyCode.Escape) && paused) 
+		{
+			Time.timeScale = 1f;
+			paused = false;
+			// take down menu
+		}
+	}
+}
