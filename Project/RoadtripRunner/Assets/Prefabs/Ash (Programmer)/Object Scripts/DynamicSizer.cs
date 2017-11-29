0624b0d13f63f137389e456e3568a9543fcba127
AIE\s170958 <krianoto@gmail.com>
2017-11-29 13:15:22 +1030

    Working on the dynamic platforms. DO NOT MERGE

--- Project/RoadtripRunner/Assets/Prefabs/Ash (Programmer)/Object Scripts/DynamicSizer.cs
+++ Project/RoadtripRunner/Assets/Prefabs/Ash (Programmer)/Object Scripts/DynamicSizer.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class DynamicSizer : MonoBehaviour
 {
-    public Transform firstPoint;
-    public Transform secondPoint;
+    public Vector3 firstPoint;
+    public Vector3 secondPoint;
     public float yPos;
     public float zPos;
 
-	void Start ()
-    {
-		
-	}
 	
 	void Update ()
     {
         if (firstPoint == null || secondPoint == null) { return; }
 
-        float scaleMultiplier = Vector3.Distance(firstPoint.position, secondPoint.position) / transform.lossyScale.x;
+        float scaleMultiplier = Vector3.Distance(firstPoint, secondPoint) / transform.lossyScale.x;
 
-        transform.position = new Vector3(Mathf.Lerp(firstPoint.position.x, secondPoint.position.x, 0.5f), yPos, zPos);
+        transform.position = new Vector3(Mathf.Lerp(firstPoint.x, secondPoint.x, 0.5f), yPos, zPos);
         transform.localScale = new Vector3(transform.localScale.x * scaleMultiplier, transform.localScale.y, transform.localScale.z);
     }
 }
--- Project/RoadtripRunner/Assets/Prefabs/Designer 1 PREFABS/Script/PlatformController.cs
+++ Project/RoadtripRunner/Assets/Prefabs/Designer 1 PREFABS/Script/PlatformController.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlatformController : MonoBehaviour {
 
-	public Transform m_platformFocalPoint;
+    public Transform m_platformFocalPoint1;
+    public Transform m_platformFocalPoint2;
 
-	public Transform m_activeCamera;
+    public Transform m_activeCamera;
     public Transform SetCamera { get; set; }
 
 	public float m_speed;
@@ -23,54 +24,75 @@ public class PlatformController : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		m_rigidbody = GetComponent<Rigidbody>();
 		SetSpeed = m_speed;
 		SetCamera = m_activeCamera;
 	}
 	
 
-	void FixedUpdate() {
-		SetDirection();
+	void FixedUpdate()
+    {
+		//SetDirection();
         RaycastHit hit;
         //raycast from camera to platformFocalPoint
-        Debug.DrawRay(m_activeCamera.position, (m_platformFocalPoint.position - m_activeCamera.position), Color.red);
+        Debug.DrawRay(m_activeCamera.position, (m_platformFocalPoint1.position - m_activeCamera.position), Color.red);
+        Debug.DrawRay(m_activeCamera.position, (m_platformFocalPoint2.position - m_activeCamera.position), Color.red);
 
 
-		Vector3 viewPos = Camera.main.WorldToViewportPoint(startOfPlatform.position);
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(startOfPlatform.position);
 		Vector3 viewPos2 = Camera.main.WorldToViewportPoint (endOfPlatform.position);
 
 		LayerMask layerMask = (1<<11);
-		if(viewPos.x > 0 || viewPos2.x > 0) {
-			//should ignore collisions with layermask
-			if(Physics.Raycast(m_activeCamera.position, (m_platformFocalPoint.position - m_activeCamera.position), out hit, Mathf.Infinity ,~layerMask)) {
-				m_platformPos.position = hit.point;
-				Debug.DrawRay(hit.point, (m_platformFocalPoint.position - hit.point), Color.green);
+		if(viewPos.x > 0 || viewPos2.x > 0)
+        {
+            DynamicSizer Sizer = GetComponent<DynamicSizer>();
+
+            //should ignore collisions with layermask
+            if (Physics.Raycast(m_activeCamera.position, (m_platformFocalPoint1.position - m_activeCamera.position), out hit, Mathf.Infinity ,~layerMask))
+            {
+                Sizer.firstPoint = hit.point;
+				Debug.DrawRay(hit.point, (m_platformFocalPoint1.position - hit.point), Color.green);
 			}
-		}
+            if (Physics.Raycast(m_activeCamera.position, (m_platformFocalPoint2.position - m_activeCamera.position), out hit, Mathf.Infinity, ~layerMask))
+            {
+                Sizer.secondPoint = hit.point;
+                Debug.DrawRay(hit.point, (m_platformFocalPoint2.position - hit.point), Color.green);
+            }
+        }
 
-		if(transform.position.z == m_platformPos.position.z) {
-			SetSpeed = 0;
-		} else {
-			SetSpeed = m_speed;
-		}
+		//if(transform.position.z == m_platformPos.position.z)
+  //      {
+		//	SetSpeed = 0;
+		//} else
+  //      {
+		//	SetSpeed = m_speed;
+		//}
 
-		//actual movement code
-		m_rigidbody.MovePosition(m_rigidbody.position + m_direction * m_speed * Time.deltaTime);
+		////actual movement code
+		//m_rigidbody.MovePosition(m_rigidbody.position + m_direction * m_speed * Time.deltaTime);
 	}
 
 	//sets the direction of the platform movement
-	void SetDirection() {
+	void SetDirection()
+    {
 		m_direction = (m_platformPos.position - m_rigidbody.position);
 	}
 
 	//testing pos stuff
-	void OnDrawGizmos() {
-		if(transform.position.z > m_platformPos.position.z) {
+	void OnDrawGizmos()
+    {
+		if(transform.position.z > m_platformPos.position.z)
+        {
 			Gizmos.color = Color.green;
-		}else if(transform.position.z < m_platformPos.position.z) {
+		}
+        else if(transform.position.z < m_platformPos.position.z)
+        {
 			Gizmos.color = Color.red;
-		}else if(transform.position.z == m_platformPos.position.z) {
+		}
+        else if(transform.position.z == m_platformPos.position.z)
+        {
 			Gizmos.color = Color.blue;
 		}
 		Gizmos.DrawWireCube(transform.position, m_platformPos.localScale);
